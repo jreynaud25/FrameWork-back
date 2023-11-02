361bb0213ff49c6cd99cd5f20fd882b11d9f9daf
DamdamTouch <damien.audrezet@icloud.com>
2023-11-02 16:27:15 +0100

    adding response

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 // require('dotenv/config')
-require("dotenv").config()
+require("dotenv").config();
 // Connect to the database
-require("./config/dbConfig")
+require("./config/dbConfig");
 // We need express
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 // Need the app
-const app = express()
+const app = express();
 // Configuration of the app
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Authorize everyone
 // app.use(cors())
 // Authorize just our frontend
 app.use(
-	cors({
-		origin: process.env.FRONTEND_URL,
-	})
-)
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
 /**
  * Quick example of middlewares
@@ -34,7 +34,7 @@ app.use(
 
 // Here we are importing the index router
 // All the request are handled in the subsequent routes
-app.use("/api", require("./routes/index.routes"))
+app.use("/api", require("./routes/index.routes"));
 /**
  *
  * ! Traffic handler
@@ -63,23 +63,23 @@ app.use("/api", require("./routes/index.routes"))
  */
 
 app.use("*", (req, res, next) => {
-	res.json({ message: "That's a 404 right here..." })
-})
+  res.status(200).json({ message: "That's a 404 right here..." });
+});
 
 app.use((err, req, res, next) => {
-	console.log(err)
-	if (err.name === "CastError") {
-		return res.status(400).json({
-			message: "Cast error",
-			details: "Make sure you are sending correct informations",
-		})
-	}
+  console.log(err);
+  if (err.name === "CastError") {
+    return res.status(400).json({
+      message: "Cast error",
+      details: "Make sure you are sending correct informations",
+    });
+  }
 
-	if (err.name === "TokenExpiredError") {
-		return res.status(401).json({ message: "Token expired" })
-	}
-	res.status(500).json({ error: err, message: err.message })
-})
+  if (err.name === "TokenExpiredError") {
+    return res.status(401).json({ message: "Token expired" });
+  }
+  res.status(500).json({ error: err, message: err.message });
+});
 
 // function modifytheRequest(req, res, next) {
 // 	req.cat = { name: "Illiu" };
@@ -92,5 +92,5 @@ app.use((err, req, res, next) => {
 // }
 
 app.listen(process.env.PORT, () =>
-	console.log(`Server running on http://localhost:${process.env.PORT}`)
-)
+  console.log(`Server running on http://localhost:${process.env.PORT}`)
+);
