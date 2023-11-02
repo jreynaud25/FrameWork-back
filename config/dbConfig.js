72f80cc80aec658d7df4987ee8888c1c322c5be6
DamdamTouch <damien.audrezet@icloud.com>
2023-11-02 16:04:33 +0100

    adding vercel CORS

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
 
-const URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/framework'
-
+const URI = process.env.MONGODB_URI || "mongodb://localhost:27017/framework";
 
 if (!URI) {
-	throw Error("Could not find MONGODB_URI in the .env file.");
+  throw Error("Could not find MONGODB_URI in the .env file.");
 }
 
 mongoose
-	.connect(URI)
-	.then((db) => {
-		console.log(`Connected to ${db.connection.name}`);
-	})
-	.catch((error) => {
-		console.log(error.message);
-	});
+  .connect(URI)
+  .then((db) => {
+    console.log(`Connected to ${db.connection.name}`);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
