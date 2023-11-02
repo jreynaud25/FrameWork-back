fa22c13bd331ed82af10ffe622668801647d7326
DamdamTouch <damien.audrezet@icloud.com>
2023-11-02 16:48:26 +0100

    adding response

--- api/index.js
+++ api/index.js
@@ -22,6 +22,18 @@ app.use(
 
 app.get("/api", (req, res) => {
   const path = `/api/item/${v4()}`;
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
